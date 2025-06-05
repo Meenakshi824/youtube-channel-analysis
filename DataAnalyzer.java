f14e451cebd2212792856a7fbf8f7e848876187a
Meenakshi <meenakshish824@gmail.com>
2025-06-05 12:46:28 +0530

    Delete DataAnalyzer.java

--- DataAnalyzer.java
+++
@@ -1,9 +0,0 @@
-double totalViews = 0;
-double maxViews = 0;
-for (String[] row : rows) {
-    double views = Double.parseDouble(row[4]);
-    totalViews += views;
-    if (views > maxViews) maxViews = views;
-}
-System.out.println("Total Views: " + totalViews);
-System.out.println("Max Viewed Video: " + maxViews);
