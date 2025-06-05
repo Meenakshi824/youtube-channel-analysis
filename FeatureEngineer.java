59fa111145f5cf1be60550819015f114f52bb720
Meenakshi <meenakshish824@gmail.com>
2025-06-05 12:46:40 +0530

    Delete FeatureEngineer.java

--- FeatureEngineer.java
+++
@@ -1,7 +0,0 @@
-double likes = Double.parseDouble(row[5]);
-double dislikes = Double.parseDouble(row[6]);
-double comments = Double.parseDouble(row[7]);
-double views = Double.parseDouble(row[4]);
-
-double likeRatio = likes / (dislikes + 1);
-double viewsPerComment = views / (comments + 1);
