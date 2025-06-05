d6ac3226741727c2aac37828f038a755d7ac28c2
Meenakshi <meenakshish824@gmail.com>
2025-06-05 12:46:50 +0530

    Delete OutlierHandler.java

--- OutlierHandler.java
+++
@@ -1,8 +0,0 @@
-List<Double> viewList = new ArrayList<>();
-for (String[] row : rows) {
-    viewList.add(Double.parseDouble(row[4]));
-}
-Collections.sort(viewList);
-double q1 = viewList.get(viewList.size() / 4);
-double q3 = viewList.get(3 * viewList.size() / 4);
-double iqr = q3 - q1;
